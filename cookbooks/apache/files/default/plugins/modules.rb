141d737dfa5b6b06093b8ad93f54e301e32ab6bf
nathenharvey <nharvey@chef.io>
2015-02-06 10:46:42 +0000

    Update the ohai plugin

--- cookbooks/apache/files/default/plugins/modules.rb
+++ cookbooks/apache/files/default/plugins/modules.rb
@@ -3,9 +3,18 @@ Ohai.plugin(:Apache) do
   provides "apache/modules"
 
   collect_data(:default) do
-    modules = Mixlib::ShellOut.new("apachectl -t -D DUMP_MODULES")
-    modules.run_command
     apache Mash.new
-    apache[:modules] = modules.stdout
+    apache[:modules] = {:static => [], :shared => []}
+    modules = shell_out("apachectl -t -D DUMP_MODULES")
+    modules.stdout.each_line do |line|
+      fullkey, value = line.split(/\(/, 2).map {|i| i.strip}
+      apache_mod = fullkey.gsub(/_module/,"")
+      dso_type = value.to_s.chomp("\)")
+      if dso_type.match(/shared/)
+        apache[:modules][:shared] << apache_mod
+      elsif dso_type.match(/static/)
+        apache[:modules][:static] << apache_mod
+      end
+    end
   end
 end
