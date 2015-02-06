d4d723b83e8a63191cfe335072c2c6173133746f
nathenharvey <nharvey@chef.io>
2015-02-06 14:25:08 +0000

    ChefSpec for motd

---
+++ cookbooks/motd/spec/unit/default_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper.rb'
+
+describe 'motd::default' do
+  let(:chef_run) { ChefSpec::ServerRunner.new.converge(described_recipe) }
+
+  it 'creates an motd correctly' do
+    expect(chef_run).to create_template('/etc/motd').with(
+      :user => 'root',
+      :group => 'root',
+      :mode => '0644'
+    )
+  end
+end
