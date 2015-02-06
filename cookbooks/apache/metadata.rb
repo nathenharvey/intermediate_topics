05734be2b061cca0ecaa381aa79ddd316a85742b
nathenharvey <nharvey@chef.io>
2015-02-06 12:25:46 +0000

    Foodcritic lab

--- cookbooks/apache/metadata.rb
+++ cookbooks/apache/metadata.rb
@@ -1,6 +1,6 @@
 name             'apache'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'Chef Software, Inc'
+maintainer_email 'nharvey@chef.io'
 license          'All rights reserved'
 description      'Installs/Configures apache'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
